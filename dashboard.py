73f230f098ea0fd311b0ca31a31f7c0c8ef27c9b
harshj3915 <hjain3915@gmail.com>
2025-06-18 09:57:46 +0400

    Update .gitignore to include 'q/' directory and change app.run to debug=False in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -884,4 +884,4 @@ def update_tables(invoice_days, weeks, brands, idgs, types, selected_weekday):
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    app.run(debug=False, port=8050)
